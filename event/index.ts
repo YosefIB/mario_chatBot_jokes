9a76896297e27ed9bea7f4c8030bb9ec9e02d34e
Yosef Ibrahim <yosefib88@gmail.com>
2024-09-05 22:15:50 +0300

    123

---
+++ event/index.ts
@@ -0,0 +1,9 @@
+const button = document.getElementById('button');
+
+if (button) {
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+}
+
+alert('Button clicked!');
