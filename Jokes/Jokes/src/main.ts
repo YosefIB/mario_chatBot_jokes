1cecfad5ba31cdf302c0a6b6a504b9710820e56b
Yosef Ibrahim <yosefib88@gmail.com>
2024-09-21 12:39:25 +0300

    5454
    5454

---
+++ Jokes/Jokes/src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+import { render_jokes } from './view/render_jokes.ts'
+
+render_jokes();
---
+++ Jokes/Jokes/src/view/render_jokes.ts
@@ -0,0 +1,7 @@
+import { insert_jokes } from "../controllers/insert_jokes";
+import { jokes } from "../controllers/insert_jokes";
+
+export function render_jokes() {
+   
+
+}
