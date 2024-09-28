d0a9bbc661bb157d53734b18af522238d2e67c8b
Yosef Ibrahim <yosefib88@gmail.com>
2024-09-28 12:44:39 +0300

    123
    123

---
+++ About_me/About_Me/src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
