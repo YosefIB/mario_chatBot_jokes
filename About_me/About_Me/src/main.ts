5d73760791bf065893896f5091ba8b16da418183
Yosef Ibrahim <yosefib88@gmail.com>
2024-10-01 02:09:18 +0300

    123
    123

--- About_me/About_Me/src/main.ts
+++ About_me/About_Me/src/main.ts
@@ -2,6 +2,14 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import { render_person } from './view/index.ts'
+import { change_pic } from './controller/index.ts'
 
+const btn = document.getElementById("print_to_screen")
+if (btn) {
+  btn.addEventListener("click", () => {
+    render_person()
+  });
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+change_pic();
