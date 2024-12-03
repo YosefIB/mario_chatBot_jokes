131d57309790f571205672b3712a2c19b4e6cd50
Yosef Ibrahim <yosefib88@gmail.com>
2024-12-03 02:45:08 +0200

    add login&regitser pop ups

--- AI-Shop/2.12.24/public/dist/index.js
+++ AI-Shop/2.12.24/public/dist/index.js
@@ -4,9 +4,16 @@ if (loginRegisterButton && openMenu) {
     loginRegisterButton.addEventListener('mouseover', function () {
         openMenu.style.display = 'block';
     });
-    var closeMenu = function () {
-        openMenu.style.display = 'none';
-    };
+    function closeMenu() {
+        try {
+            if (openMenu) {
+                openMenu.style.display = 'none';
+            }
+        }
+        catch (error) {
+            console.error('An error occurred:', error.message);
+        }
+    }
     loginRegisterButton.addEventListener('mouseleave', closeMenu);
     openMenu.addEventListener('mouseleave', closeMenu);
     openMenu.addEventListener('mouseover', function () {
@@ -19,6 +26,14 @@ var closeButton = document.getElementById('closeChat');
 var sendButton = document.getElementById('sendMessage');
 var chatInput = document.getElementById('chatInput');
 var messagesContainer = document.getElementById('messages');
+var loginPage = document.getElementById('popupLogin'); /* אני תופס את הפופ אפ כניסה כדי לא להציג אותו בהתחלה */
+if (!loginPage)
+    throw new Error('Popup element not found');
+loginPage.style.display = 'none';
+var registerPage = document.getElementById('popupRegister'); /* אני תופס את הפופ אפ הרשמה כדי לא להציג אותו בהתחלה */
+if (!registerPage)
+    throw new Error('Popup element not found');
+registerPage.style.display = 'none';
 chatIcon.addEventListener('click', function () {
     chatWindow.style.display = 'flex';
 });
@@ -42,4 +57,59 @@ function simulateAIResponse(userMessage) {
     aiMessageElement.textContent = "AI Response: " + userMessage;
     messagesContainer.appendChild(aiMessageElement);
     messagesContainer.scrollTop = messagesContainer.scrollHeight;
+}
+function login() {
+    try {
+        var openLoginPage = document.getElementById('login');
+        var closeLoginButton = document.getElementById('closePopupLoginButton');
+        if (!openLoginPage || !closeLoginButton || !loginPage)
+            throw new Error('Open/close buttons not found');
+        if (!openLoginPage || !closeLoginButton)
+            throw new Error('Open/close buttons not found');
+        // פעולה לפתיחת הפופ-אפ
+        openLoginPage.onclick = function () {
+            loginPage.style.display = 'flex'; // מציג את הפופ-אפ
+        };
+        // פעולה לסגירת הפופ-אפ
+        closeLoginButton.onclick = function () {
+            loginPage.style.display = 'none'; // מסתיר את הפופ-אפ
+        };
+        // אפשרות לסגור את הפופ-אפ כשעושים קליק מחוץ לו
+        window.onclick = function (event) {
+            if (event.target === loginPage) {
+                loginPage.style.display = 'none';
+            }
+        };
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+function register() {
+    try {
+        var openRegisterPage = document.getElementById('22registerr');
+        var closeRegisterButton = document.getElementById('closePopupRegisterButton');
+        openRegisterPage.onclick = function () {
+            registerPage.style.display = 'flex'; // מציג את הפופ-אפ
+        };
+        // פעולה לסגירת הפופ-אפ
+        closeRegisterButton.onclick = function () {
+            registerPage.style.display = 'none'; // מסתיר את הפופ-אפ
+        };
+        // אפשרות לסגור את הפופ-אפ כשעושים קליק מחוץ לו
+        window.onclick = function (event) {
+            if (event.target === registerPage) {
+                registerPage.style.display = 'none';
+            }
+        };
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+function normalLogin() {
+    alert("אנחנו עובדים על זה");
+}
+function googleLogin() {
+    alert("סבלנות בבקשה, אנחנו על זה");
 }
--- AI-Shop/2.12.24/public/index.ts
+++ AI-Shop/2.12.24/public/index.ts
@@ -6,10 +6,17 @@ if (loginRegisterButton && openMenu) {
     openMenu.style.display = 'block';
   });
 
-  const closeMenu = () => {
-    openMenu.style.display = 'none';
-  };
+function closeMenu() {
 
+  try{
+    if (openMenu) {
+      openMenu.style.display = 'none';
+    }
+  }
+  catch(error){
+    console.error('An error occurred:', error.message);
+  }
+}
   loginRegisterButton.addEventListener('mouseleave', closeMenu);
   openMenu.addEventListener('mouseleave', closeMenu);
 
@@ -24,6 +31,19 @@ const closeButton = document.getElementById('closeChat') as HTMLElement;
 const sendButton = document.getElementById('sendMessage') as HTMLElement;
 const chatInput = document.getElementById('chatInput') as HTMLInputElement;
 const messagesContainer = document.getElementById('messages') as HTMLElement;
+
+
+const loginPage = document.getElementById('popupLogin') as HTMLElement   /* אני תופס את הפופ אפ כניסה כדי לא להציג אותו בהתחלה */
+if (!loginPage)
+  throw new Error('Popup element not found');
+
+loginPage.style.display = 'none';
+
+const registerPage = document.getElementById('popupRegister') as HTMLElement   /* אני תופס את הפופ אפ הרשמה כדי לא להציג אותו בהתחלה */
+if (!registerPage)
+  throw new Error('Popup element not found');
+
+registerPage.style.display = 'none';
 
 chatIcon.addEventListener('click', () => {
   chatWindow.style.display = 'flex'; 
@@ -54,4 +74,77 @@ function simulateAIResponse(userMessage: string): void {
   messagesContainer.appendChild(aiMessageElement);
 
   messagesContainer.scrollTop = messagesContainer.scrollHeight;
+}
+
+function login(){
+  try{
+    const openLoginPage = document.getElementById('login')  as HTMLElement;
+    const closeLoginButton = document.getElementById('closePopupLoginButton')  as HTMLElement;
+    if (!openLoginPage ||!closeLoginButton || !loginPage)
+      throw new Error('Open/close buttons not found');
+
+
+if (!openLoginPage || !closeLoginButton)
+  throw new Error('Open/close buttons not found');
+        // פעולה לפתיחת הפופ-אפ
+
+        openLoginPage.onclick = function() {
+          loginPage.style.display = 'flex';  // מציג את הפופ-אפ
+        };
+    
+        // פעולה לסגירת הפופ-אפ
+        closeLoginButton.onclick = function() {
+          loginPage.style.display = 'none';  // מסתיר את הפופ-אפ
+        };
+    
+        // אפשרות לסגור את הפופ-אפ כשעושים קליק מחוץ לו
+        window.onclick = function(event) {
+          if (event.target === loginPage) {
+            loginPage.style.display = 'none';
+          }
+         
+        };
+    
+  }
+  catch(err){
+    console.error(err);
+  }
+}
+
+function register(){
+  try{
+
+    const openRegisterPage = document.getElementById('22registerr')  as HTMLElement;
+    const closeRegisterButton = document.getElementById('closePopupRegisterButton')  as HTMLElement;
+
+
+        openRegisterPage.onclick = function() {
+          registerPage.style.display = 'flex';  // מציג את הפופ-אפ
+        };
+
+        // פעולה לסגירת הפופ-אפ
+        closeRegisterButton.onclick = function() {
+          registerPage.style.display = 'none';  // מסתיר את הפופ-אפ
+        };
+    
+        // אפשרות לסגור את הפופ-אפ כשעושים קליק מחוץ לו
+        window.onclick = function(event) {
+          if (event.target === registerPage) {
+            registerPage.style.display = 'none';
+          }
+         
+        };
+    
+  }
+  catch(err){
+    console.error(err);
+  }
+}
+
+function normalLogin(){
+  alert("אנחנו עובדים על זה");
+}
+
+function googleLogin(){
+  alert("סבלנות בבקשה, אנחנו על זה");
 }
