7fd9541496227908a1b3a50b6cf5e8e96d9a13af
Yosef Ibrahim <yosefib88@gmail.com>
2024-12-30 22:06:32 +0200

    1
    1

--- AI-shop/src/routes/dist/adminRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var setAdmin_1 = require("../controllers/admin/setAdmin");
-var adminRouter = express_1["default"].Router();
-adminRouter.post("/register-admin", setAdmin_1.registerAdmin);
-adminRouter.post("/login-admin", setAdmin_1.loginAdmin);
-exports["default"] = adminRouter;
